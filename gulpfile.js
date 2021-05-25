f0e30bb228cfaacaf95a570fefe06a0f844789e3
IliaAstaxov <AstaxovIlia@gmail.com>
2021-05-25 19:14:12 +0300

    cat-energy

---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const htmlreplace = require("gulp-html-replace");
+const minify = require("gulp-minifier");
+
+// Styles
+
+const styles = () => {
+  return gulp
+    .src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("styles.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
+};
+
+exports.styles = styles;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: "source",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/*.html", gulp.series("html")).on("change", sync.reload);
+};
+
+exports.default = gulp.series(styles, server, watcher);
+
+// ImageMin
+
+const images = () => {
+  return gulp
+    .src("source/img/**/*.{jpg,png,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo(),
+      ])
+    );
+};
+
+exports.images = images;
+
+//Sprite
+
+const sprite = () => {
+  return gulp
+    .src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+};
+
+exports.sprite = sprite;
+
+//Html
+
+const html = () => {
+  return gulp
+    .src("source/*.html")
+    .pipe(
+      htmlreplace({
+        css: "css/styles.min.css",
+      })
+    )
+    .pipe(gulp.dest("build/"));
+};
+
+exports.html = html;
+
+//Copy
+
+const copy = () => {
+  return gulp
+    .src(
+      [
+        "source/fonts/**/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/*.ico",
+        "source/*.html",
+      ],
+      {
+        base: "source",
+      }
+    )
+    .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+//Delete
+
+const clean = () => {
+  return del("build");
+};
+
+//Minify HTML, JS
+gulp.task("minify", function () {
+  return gulp
+    .src("source/**/*")
+    .pipe(
+      minify({
+        minify: true,
+        minifyHTML: {
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+        },
+        // minifyJS: {
+        //   sourceMap: true
+        // },
+        getKeptComment: function (content, filePath) {
+          var m = content.match(/\/\*![\s\S]*?\*\//gim);
+          return (m && m.join("\n") + "\n") || "";
+        },
+      })
+    )
+    .pipe(gulp.dest("build"));
+});
+
+const build = gulp.series(clean, copy, styles, sprite, html);
+exports.build = build;
